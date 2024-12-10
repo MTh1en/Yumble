7fc07da6399023048ddbfc50a451258a629e99c9
MTh1en <mt12122003@gmail.com>
2024-12-10 09:46:17 +0700

    Yumble init project

---
+++ src/main/java/MThien/Yumble/YumbleApplication.java
@@ -0,0 +1,13 @@
+package MThien.Yumble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MThien/Yumble/YumbleApplicationTests.java
@@ -0,0 +1,13 @@
+package MThien.Yumble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
