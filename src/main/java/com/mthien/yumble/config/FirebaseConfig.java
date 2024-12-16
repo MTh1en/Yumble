abd2e4bdea8c12a417bf82a2bfd72e93a2988b42
MTh1en <mt12122003@gmail.com>
2024-12-17 06:44:34 +0700

    set up variable environment

--- src/main/java/com/mthien/yumble/config/FirebaseConfig.java
+++ src/main/java/com/mthien/yumble/config/FirebaseConfig.java
@@ -7,15 +7,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
     @Bean
     public Storage firebaseStorage() throws IOException {
-        ClassPathResource serviceAccount = new ClassPathResource("yumble-firebase-key.json");
+        String firebaseConfigPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+        FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
         return StorageOptions.newBuilder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build()
                 .getService();
     }
